da8bb770bbe929df1204f7155b78f963483b21a9
BASIL ALBADER <basilalbapro@gmail.com>
2025-05-05 22:15:59 +0200

    WIP

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,17 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import ky, { KyResponse } from 'ky';
 
 export default async function Home() {
-	const people = await fetch('http://localhost:4000/swapi/getPeople');
-	const planets = await fetch('http://localhost:4000/swapi/getPlanets');
+	const people = await ky.get('https://www.swapi.tech/api/people').json();
+	const planets = await ky.get('https://www.swapi.tech/api/planets').json();
 
-	console.log('people', people);
-	console.log('planets', planets);
 
 	return (
 		<>
 			<div className="flex flex-col gap-4">
 				<div className="flex flex-row gap-4 text-green-500">
 					<h1 className="text-2xl font-bold">People</h1>
-					{people.results.map((person: any) => (
+					{people?.results?.map((person: any) => (
 						<Card key={person.id}>
 							<CardHeader>
 								<CardTitle>{person.name}</CardTitle>
@@ -23,7 +22,7 @@ export default async function Home() {
 
 				<div className="flex flex-row gap-4 text-blue-500">
 					<h1 className="text-2xl font-bold">Planets</h1>
-					{planets.results.map((planet: any) => (
+					{planets?.results?.map((planet: any) => (
 						<Card key={planet.id}>
 							<CardHeader>
 								<CardTitle>{planet.name}</CardTitle>
